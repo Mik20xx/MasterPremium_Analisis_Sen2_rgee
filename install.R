6e95119c4564743acd667a2686cd9166ecbe0462
MasterGIS <80126056+MasterGis01@users.noreply.github.com>
2021-04-29 23:08:10 -0500

    Update install.R
    Code

--- install.R
+++ install.R
@@ -9,8 +9,21 @@ library(rgee)
 Sys.which('make')
 
 #install packages of rgee
-ee_install()
-
+rgee:: ee_install()
+    #consola
+    "Would you like to install Miniconda? [Y/n]:Y "
+    
+    "1. Removing the previous Python Environment (rgee), if it exists ..."
+    "2. Creating a Python Environment (rgee)"
+    
+    "Would you like to continues? [Y/n]:Y"
+    
+    "3. The Environment Variable 'Dirección de la capeta'was stored in the file."
+    "4. Installing the earthengine-api."
+    
+    "Do you want restart your R session? 
+          1: yes
+          2: no"
 #initializing rgee 
 ee_Initialize('juniorcalvo')
 
@@ -20,16 +33,17 @@ ee_user_info()
 ee_clean_credentials('junior')
 
 #loading a satellite image collection of GEE
-sen2<-ee$ImageCollection('COPERNICUS/S2')$
+sen2<-ee$ImageCollection('')$
   filterDate('2020-04-01', '2021-04-01')$
   filterBounds(ee$Geometry$Point(-76.924989, -11.925505))$
   filterMetadata('CLOUDY_PIXEL_PERCENTAGE', 'less_than', 5)
 
 ee_get_date_ic(sen2)
 #loading a satellite image of GEE
-s2<-'COPERNICUS/S2/20210326T151659_20210326T151713_T18LTM' %>% 
+s2<-'' %>% 
   ee$Image() %>%
   ee$Image$select(c( 'B11', 'B8A', 'B2'))
+
 #display of information
 ee_print(s2, img_band = 'B8A')
 
